5d00d081a1b9a9a7bc765c880cc154347abac1ad
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-06-19 13:47:50 +0200

    constructor injection added

--- ui/src/test/java/UITest.java
+++ ui/src/test/java/UITest.java
@@ -1,26 +1,27 @@
-import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import org.example.guice.TestModule;
 import org.example.model.User;
 import org.example.service.UserDataService;
 import org.testng.Assert;
+import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
+@Guice(modules = {TestModule.class})
 public class UITest {
 
     @Inject
-    private UserDataService userDataService;
+    public UITest(final UserDataService userDataService){
+        this.userDataService = userDataService;
+    }
 
+    private UserDataService userDataService;
     private static final String TEST_DATA_PATH =  System.getProperty("user.dir") + "/src/test/resources/"
             + System.getProperty("env") + "/users.json";
     private static final String TEST_USER_TYPE =  "suspended";
 
     @Test
     public void test() {
-        Injector injector = Guice.createInjector(new TestModule());
-        UserDataService obj = injector.getInstance(UserDataService.class);
-        User user = obj.getTestUserByType(TEST_USER_TYPE, TEST_DATA_PATH);
+        User user = userDataService.getTestUserByType(TEST_USER_TYPE, TEST_DATA_PATH);
         Assert.assertEquals(user.getUsername(), "charlie");
         Assert.assertEquals(user.getPassword(), "qwerty789");
         Assert.assertEquals(user.getUsertype(), TEST_USER_TYPE);
