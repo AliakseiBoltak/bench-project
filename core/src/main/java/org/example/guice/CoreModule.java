a79a5c7bf2c777ba827c34635c18a83140d0c6f2
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-15 14:32:37 +0200

    extra fields removed

--- core/src/main/java/org/example/guice/CoreModule.java
+++ core/src/main/java/org/example/guice/CoreModule.java
@@ -3,16 +3,9 @@ package org.example.guice;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import org.example.config.ConfigConstants;
 import org.example.config.ConfigLoader;
 
 public class CoreModule extends AbstractModule {
-
-    private final String env;
-
-    public CoreModule() {
-        this.env = ConfigConstants.ENV;
-    }
 
     @Provides
     @Singleton
