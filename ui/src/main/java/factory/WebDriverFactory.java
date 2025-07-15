2f4ea9090ac4578332b76bba1d1c874c5092007b
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-15 14:30:05 +0200

    extra space removed

--- ui/src/main/java/factory/WebDriverFactory.java
+++ ui/src/main/java/factory/WebDriverFactory.java
@@ -5,7 +5,7 @@ import com.codeborne.selenide.Configuration;
 public class WebDriverFactory {
 
     private static final String DEFAULT_BROWSER = "chrome";
-    private static final String BROWSER  = System.getProperty("browser", DEFAULT_BROWSER).toLowerCase();
+    private static final String BROWSER = System.getProperty("browser", DEFAULT_BROWSER).toLowerCase();
 
     public static void initBrowser() {
         Configuration.browser = switch (BROWSER) {
