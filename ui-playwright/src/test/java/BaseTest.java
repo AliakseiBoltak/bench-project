f5f719b2391e4399edba59bdd801c74d9d33c845
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-18 13:13:21 +0200

    maven.yml fixed

--- ui-playwright/src/test/java/BaseTest.java
+++ ui-playwright/src/test/java/BaseTest.java
@@ -28,7 +28,7 @@ public class BaseTest {
     public void setUp() {
         Allure.step("Open browser");
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         context = browser.newContext();
         page = context.newPage();
         AllurePlaywrightListener.setPage(page);
