d293b32ddedf1ad78b560eb3cd9f5f56dc677d5b
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-19 13:20:36 +0200

    small fix

--- ui/src/main/java/pages/HomePage.java
+++ ui/src/main/java/pages/HomePage.java
@@ -8,7 +8,7 @@ public class HomePage {
 
     public boolean checkIfHomePageLoaded() {
         return $(By.xpath("./descendant::span[@class='Button-label' and text()='New'][1]")).isDisplayed()
-                & $(By.xpath("//h2[text()='Home']")).isDisplayed();
+                && $(By.xpath("//h2[text()='Home']")).isDisplayed();
     }
 
 }
