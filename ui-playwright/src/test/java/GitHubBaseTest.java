9e4814e99a32eb28869962102183ef13eca80166
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-21 16:43:49 +0200

    cleanup

--- ui-playwright/src/test/java/GitHubBaseTest.java
+++ ui-playwright/src/test/java/GitHubBaseTest.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Guice;
 import pages.GitHubLoginPage;
-import pages.GitHubMainPage;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
