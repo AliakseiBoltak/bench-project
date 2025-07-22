b2eb31c5d249739c993ccfaf5ec411299d4d26b2
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-22 14:04:32 +0200

    RandomUserGenerator updates in core module

--- core/src/main/java/org/example/config/Constants.java
+++ core/src/main/java/org/example/config/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
 
     public static final String USERS_TEST_DATA_PATH = System.getProperty("user.dir") +
             "/src/test/resources/users/users.json";
+
+    public static final String GENERATED_USERS_PATH = System.getProperty("user.dir") +
+            "/src/main/resources/users/users.json";
 }
--- core/src/main/java/org/example/utils/RandomUserGenerator.java
+++ core/src/main/java/org/example/utils/RandomUserGenerator.java
@@ -1,11 +1,16 @@
 package org.example.utils;
 
+import org.example.config.Constants;
 import org.example.constants.UserTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,12 +19,38 @@ public class RandomUserGenerator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RandomUserGenerator.class);
 
+    private static final int USERNAME_OFFSET = 1000;
+    private static final int USERNAME_RANGE = 9000;
+    private static final int PASSWORD_OFFSET = 100000;
+    private static final int PASSWORD_RANGE = 900000;
+    private static final int DEFAULT_USER_COUNT = 5;
+
     public static void main(String[] args) throws IOException {
-        int userCount = args.length > 0 ? Integer.parseInt(args[0]) : 5;
+        int userCount = DEFAULT_USER_COUNT;
+
+        if (args.length > 0) {
+            try {
+                userCount = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                LOGGER.warn("Invalid input for user count '{}'. Falling back to default value: {}", args[0],
+                        DEFAULT_USER_COUNT);
+            }
+        }
 
         String outPath = args.length > 1
                 ? args[1]
-                :  System.getProperty("user.dir") + "/src/main/resources/users.json";
+                : Constants.GENERATED_USERS_PATH;
+
+        Path outPathObj = Paths.get(outPath);
+        File parentDir = outPathObj.getParent().toFile();
+        if (!parentDir.exists()) {
+            boolean created = parentDir.mkdirs();
+            if (created) {
+                LOGGER.info("Created missing parent directories for output path: {}", parentDir.getAbsolutePath());
+            } else {
+                LOGGER.warn("Failed to create parent directories for output path: {}", parentDir.getAbsolutePath());
+            }
+        }
 
         Random random = new Random();
         UserTypes[] userTypes = UserTypes.values();
@@ -28,8 +59,8 @@ public class RandomUserGenerator {
                 .mapToObj(i -> {
                     UserTypes selectedType = userTypes[random.nextInt(userTypes.length)];
                     String usertype = selectedType.name().toLowerCase();
-                    String username = usertype + (1000 + random.nextInt(9000));
-                    String password = "pwd" + (100000 + random.nextInt(900000));
+                    String username = usertype + (USERNAME_OFFSET + random.nextInt(USERNAME_RANGE));
+                    String password = "pwd" + (PASSWORD_OFFSET + random.nextInt(PASSWORD_RANGE));
                     return String.format("  {\"usertype\": \"%s\", \"username\": \"%s\", \"password\": \"%s\"}",
                             usertype, username, password);
                 })
@@ -41,9 +72,7 @@ public class RandomUserGenerator {
         LOGGER.info("Generated {} users at {}", userCount, outPath);
 
         LOGGER.info("Available user types:");
-        for (UserTypes type : userTypes) {
-            LOGGER.info(" - {}", type);
-        }
+        Arrays.stream(userTypes).forEach(type -> LOGGER.info(" - {}", type));
     }
 
 }
