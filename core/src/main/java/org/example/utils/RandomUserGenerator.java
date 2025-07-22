3871406d8cfae567d75cbe42b4f1f129f2a12d73
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-22 15:46:23 +0200

    RandomUserGenerator cleanup

--- core/src/main/java/org/example/utils/RandomUserGenerator.java
+++ core/src/main/java/org/example/utils/RandomUserGenerator.java
@@ -12,7 +12,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
