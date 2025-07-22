86c8daacea5415a7ee6f4f18ad95ddf7d1972d05
Aliaksei Boltak <Aliaksei_Boltak@epam.com>
2025-07-22 17:12:33 +0200

    GitHubLoginOfflineTest added

---
+++ ui-playwright/src/test/java/GitHubLoginOfflineTest.java
@@ -0,0 +1,39 @@
+import com.google.inject.Inject;
+import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
+import org.example.config.ConfigLoader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.GitHubLoginPage;
+
+import java.io.ByteArrayInputStream;
+
+import static constants.PathConstants.LOGIN_PATH;
+
+public class GitHubLoginOfflineTest extends GitHubBaseTest {
+
+    @Inject
+    public GitHubLoginOfflineTest(ConfigLoader configLoader) {
+        super(configLoader);
+    }
+
+    @Override
+    protected boolean useStoredSession() {
+        return false;
+    }
+
+    @Test
+    public void testLoginPageDoesNotLoadWhenOffline() {
+        context.setOffline(true);
+        try {
+            new GitHubLoginPage(page).navigateToLogin(gitHubUrl + LOGIN_PATH);
+            Assert.fail("Page should not load when offline");
+        } catch (PlaywrightException ex) {
+            Allure.addAttachment("Login Page is not loaded when offline:",
+                    new ByteArrayInputStream(page.screenshot()));
+            Assert.assertTrue(ex.getMessage().toLowerCase().contains("net::err_internet_disconnected")
+                            || ex.getMessage().toLowerCase().contains("timeout"),
+                    "Expected offline error or timeout, but got: " + ex.getMessage());
+        }
+    }
+}
